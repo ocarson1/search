6a8f6586e1e976aab8307b2a78be2057cb0cc5f0
owen carson <owen_carson@brown.edu>
2022-04-26 21:12:20 -0400

    sfdfs

--- indexer.py
+++ indexer.py
@@ -1,9 +1,28 @@
 import xml.etree.ElementTree as et
+import file_io
+import re
 
 class Indexer:
     def __init__(self, xml, titles, docs, words):
-        root = et.parse('wikis/SmallWiki.xml').getroot()
-        to_return = {{}}
+        root = et.parse(xml).getroot()
+        to_return = {}
+        pg_maxes = {}
         for page in root:
-            page_id = int(page.find('id').text)
+            pg_id = int(page.find('id').text)
+            n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
+            words = re.findall(n_regex, page.find('text').text)
+            for word in words:
+                if word not in to_return.keys():
+                    to_return[word] = {}
+                    to_return[word][pg_id] = 1
+                else:
+                    if pg_id not in to_return[word].keys():
+                        to_return[word][pg_id] = 1
+                    else:
+                        to_return[word][pg_id] += 1
+                
+            [print(to_return)]
+            ## need to implement pg_maxes
+            ## will eventually call file_io method with dictionary and titles as parameter etc.
+
             
