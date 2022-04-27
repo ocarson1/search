4acde1ef4e2eba81d52bd5f907a5c8d14173c3ef
efiaawuah <efiaawuah@outlook.com>
2022-04-27 18:34:40 -0400

    save

--- indexer.py
+++ indexer.py
@@ -1,3 +1,4 @@
+from os import link
 import xml.etree.ElementTree as et
 import file_io
 import re
@@ -16,24 +17,39 @@ class Indexer:
         doc_max_freqs = {}
         self.weight_dict = {}
         stemmer = PorterStemmer()
+        ids_to_titles = {}
+        titles_to_ids = {}
+        ids_to_pgrank = {}
+        self.pg_links = {}
+        weight_dict = {}
+        
 
         for page in root:
             pg_id = int(page.find('id').text)
             title: str = (page.find('title').text)
+            title = title.strip()
+            ids_to_titles.update({pg_id : title})
+            titles_to_ids.update({title : pg_id})
+
             n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
             words = re.findall(n_regex, title) + (re.findall(n_regex, page.find('text').text)) #Note: currently includes numbers. not sure if issue or not but handout says it is up to our own discretion
-            print(words)
             for word in words:
+
                 word_lst = [word] 
 
                 if (re.search('\[\[[^\[]+?\]\]', word) != None): 
-                    word.strip('[]')
+                    word = word.strip('[]')
                     link_to = word
                     word_lst = [word]
                     if '|' in word: 
                         link_to = (word.split('|'))[0]
                         word = (word.split('|'))[1]
-                        word_lst = re.findall(n_regex, word)
+                    word_lst = re.findall(n_regex, word) 
+                    if title not in pg_links.keys():
+                        pg_links[title] = set()
+                        pg_links[title].add(link_to)
+                    else:
+                        pg_links[title].add(link_to)
 
                 for w in word_lst:
                     w = w.lower()
@@ -52,17 +68,35 @@ class Indexer:
                         doc_max_freqs[pg_id] = 1
                     elif to_return[w][pg_id] > doc_max_freqs[pg_id]:
                         doc_max_freqs[pg_id] = to_return[w][pg_id]
-                    
-        print(to_return)
+
         for word in to_return:
             for pg_id in to_return[word]:
                 to_return[word][pg_id] = (to_return[word][pg_id] / doc_max_freqs[pg_id]) * math.log(len(doc_max_freqs) / len(to_return[word]))                
-        print(to_return)
-        print(doc_max_freqs)
+       
+        for title in titles_to_ids.keys(): 
+            pg_id = titles_to_ids[title]
+            weight_dict[pg_id] = {}
+            
+            if title not in pg_links:
+                for other_pg_id in ids_to_titles.keys(): 
+                    if ids_to_titles[other_pg_id] != title:
+                        weight_dict[pg_id][other_pg_id] = 0
+            else: 
+                for link_to in pg_links[title]: 
+                    link_to_id = titles_to_ids[link_to]
+                    if link_to in titles_to_ids.keys() and link_to != title: 
+                        weight_dict[pg_id][link_to_id] = 0 
 
 
-    def weightCalculator(self, j_id : int, k_id : int): 
-        
+            
+        print(weight_dict)
+
+        def weight(k_id, j_id): 
+            if ids_to_titles[k_id] in self.pg_links and k_id != j_id
+
+
+
+    
             
 
 
