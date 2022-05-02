8403bae6a2f06c93d213c192c403f59a6e076e4c
efiaawuah <efiaawuah@outlook.com>
2022-05-02 17:55:58 -0400

    sav

--- index.py
+++ index.py
@@ -12,7 +12,6 @@ from nltk.stem import PorterStemmer
 
 class Indexer:
     
-    
     def __init__(self, xml, titles_file, docs_file, words_file) -> None:
         
         root = et.parse(xml).getroot()
@@ -32,6 +31,7 @@ class Indexer:
             title = title.strip()
             self.ids_to_titles.update({pg_id : title})
             self.titles_to_ids.update({title : pg_id})
+            self.pg_links[title] = set()
 
             n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
             words = re.findall(n_regex, title) + (re.findall(n_regex, page.find('text').text)) #Note: currently includes numbers. not sure if issue or not but handout says it is up to our own discretion
@@ -48,11 +48,7 @@ class Indexer:
                         word = (word.split('|'))[1]
                     word_lst = re.findall(n_regex, word)
                     if link_to != title:
-                        if title not in self.pg_links.keys():
-                            self.pg_links[title] = set()
-                            self.pg_links[title].add(link_to)
-                        else:
-                            self.pg_links[title].add(link_to)
+                        self.pg_links[title].add(link_to)
 
                 for w in word_lst:
                     w = w.lower()
@@ -88,34 +84,40 @@ class Indexer:
 
     def calculate_weights(self):
         #weights = [[0 for i in range(0, len(self.titles_to_ids) - 1)] for j in range(0, len(self.titles_to_ids) - 1)]
-        for title in self.titles_to_ids.keys(): 
-            pg_id = self.titles_to_ids[title]
-            self.weight_dict[pg_id] = {}
-            ##
-            for title in self.titles_to_ids.keys(): 
-                other_id = self.titles_to_ids[title]
-                self.weight_dict[pg_id][other_id] = 0.15/len(self.titles_to_ids.keys())
+        #initializes weights 
+        for k_title in self.titles_to_ids.keys(): 
+            k_id = self.titles_to_ids[k_title]
+            self.weight_dict[k_id] = {}
+            for j_title in self.titles_to_ids.keys(): 
+                j_id = self.titles_to_ids[j_title]
+                self.weight_dict[k_id][j_id] = 0.15/len(self.titles_to_ids.keys())
+
         id_to_link_number = {}
-        for title in self.pg_links.keys(): 
-            id_to_link_number[self.titles_to_ids[title]] = 0
-            for link_to in self.pg_links[title]: 
-                if link_to in self.titles_to_ids.keys(): 
-                    id_to_link_number[self.titles_to_ids[title]] += 1
+        for k_id in self.weight_dict.keys():
+            k_title = self.ids_to_titles[k_id]
+            id_to_link_number[k_id] = 0
 
+            #adds 1 to n_k for each valid link 
+            for link_to in self.pg_links[k_title]: 
+                if link_to in self.titles_to_ids.keys() and link_to != k_title: 
+                    id_to_link_number[k_id] += 1
 
+            #n_k is number of pgs -1 if pg has no links
+            if id_to_link_number[k_id] == 0:
+                id_to_link_number[k_id] = len(self.ids_to_titles.keys()) - 1
+                for other_title in self.titles_to_ids.keys():
+                    if other_title != k_title:
+                        self.pg_links[k_title].add(other_title)
 
-        for k_id in self.weight_dict.keys(): #maybe we can combine this with the first loop?
-            if self.ids_to_titles[k_id] not in self.pg_links.keys():
-                #print("no links")
-                for other_id in self.weight_dict.keys():
-                    if other_id != k_id:
-                        self.weight_dict[k_id][other_id] += (1 - 0.15) * (1/ (len(self.titles_to_ids.keys()) - 1))
-            else:
-                #print("links")
-                for other_id in self.weight_dict[k_id]:           
-                    if self.ids_to_titles[other_id] in self.pg_links[self.ids_to_titles[k_id]]:
-                        self.weight_dict[k_id][other_id] += (1 - 0.15) * (1/id_to_link_number[k_id])
-                    
+            print(self.pg_links[self.ids_to_titles[k_id]])
+            print(id_to_link_number[k_id])
+
+            for j_id in self.weight_dict[k_id]:
+                if self.ids_to_titles[j_id] in self.pg_links[self.ids_to_titles[k_id]]:
+                    self.weight_dict[k_id][j_id] = self.weight_dict[k_id][j_id] + (1 - 0.15) * (1/id_to_link_number[k_id])
+
+        print(self.weight_dict)
+                
     def pagerank(self):
         delta = .001
         curr = {}
@@ -130,7 +132,6 @@ class Indexer:
                 for k in self.ids_to_titles.keys():
                     if j in self.weight_dict[k]:
                         next[j] = next[j] + self.weight_dict[k][j] * curr[k]
-        print(next)
         print(sum(next.values()))
         return next
             
